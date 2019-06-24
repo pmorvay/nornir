5e708ab81f69006524afff7d0ef77be89692deb5
Peter Morvay <pmorvay@cisco.com>
2019-06-24 21:25:31 +0200

    First commit

---
+++ First nornir.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import nornir
+from nornir import InitNornir
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks.networking import napalm_get
+
+nr = InitNornir(
+    config_file="config.yaml", dry_run=True
+)
+
+results = nr.run(
+    task=napalm_get, getters=["facts", "interfaces"]
+)
+print_result(results)
